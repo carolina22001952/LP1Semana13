8b833e7ab0209fd550aa7b7e7d738a317b3a0e9c
Carolina Bastos <carol1999b@gmail.com>
2023-06-09 10:36:34 +0100

    Add project Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                bool isPalindrome = IsPalindrome(arg);
+                Console.WriteLine($"{arg} -> {isPalindrome}");
+            }
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (input.Length < 2)
+            {
+                return true;
+            }
+
+            return IsPalindromeRecursive(input);
+        }
+
+        static bool IsPalindromeRecursive(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
